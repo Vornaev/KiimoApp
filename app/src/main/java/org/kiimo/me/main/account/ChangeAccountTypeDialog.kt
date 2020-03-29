168ea772e93a908a57db481154e9b06215d4deb6
viktor manev <vice.eu@gmail.com>
2020-03-29 17:25:31 +0200

    added validation

--- app/src/main/java/org/kiimo/me/main/account/ChangeAccountTypeDialog.kt
+++ app/src/main/java/org/kiimo/me/main/account/ChangeAccountTypeDialog.kt
@@ -2,34 +2,29 @@ package org.kiimo.me.main.account
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
-import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import kotlinx.android.synthetic.main.dialog_digit_code.view.*
 import kotlinx.android.synthetic.main.layout_edit_field_with_validation.view.*
-import kotlinx.android.synthetic.main.layout_register_deliver_account.*
+import kotlinx.android.synthetic.main.layout_edit_field_with_validation.view.imageViewValidation
+import kotlinx.android.synthetic.main.layout_edit_field_with_validation.view.textViewFieldRequired
 import kotlinx.android.synthetic.main.layout_register_deliver_account.view.*
 import kotlinx.android.synthetic.main.layout_register_personal_id.view.*
 import org.kiimo.me.R
 import org.kiimo.me.databinding.DialogChangeAccountTypeUserBinding
 import org.kiimo.me.dialogs.BaseKiimoDialog
-import org.kiimo.me.main.MainActivity
-import org.kiimo.me.main.menu.KiimoMainNavigationActivity
 import org.kiimo.me.main.menu.mainViewModel.MainMenuViewModel
 import org.kiimo.me.register.model.UserAddressDataRequest
 import org.kiimo.me.register.model.UserProfileInformationRequest
 import org.kiimo.me.register.model.UserRegisterDataRequest
 import org.kiimo.me.util.IMediaManagerImages
 import org.kiimo.me.util.MediaManager
-import org.kiimo.me.util.PreferenceUtils
 
 class ChangeAccountTypeDialog : BaseKiimoDialog() {
 
@@ -58,19 +53,20 @@ class ChangeAccountTypeDialog : BaseKiimoDialog() {
 
 
         binding.changeAccountTypeSaveButton.setOnClickListener {
+            if (validations()) {
+                if (userRequest != null) {
+                    userRequest!!.personalID = personalIDImageUrl
+                    userRequest!!.address = UserAddressDataRequest(
+                        countryCode = binding.registerDeliverLayout.layoutRegisterProfileCountryCode.EditTextFieldValidation.text.toString(),
+                        houseNumber = binding.registerDeliverLayout.layoutRegisterUserHouseNumber.EditTextFieldValidation.text.toString(),
+                        place = binding.registerDeliverLayout.layoutRegisterProfilePlace.EditTextFieldValidation.text.toString(),
+                        street = binding.registerDeliverLayout.layoutRegisterUserStreet.EditTextFieldValidation.text.toString(),
+                        zip = binding.registerDeliverLayout.layoutRegisterProfileZipCode.EditTextFieldValidation.text.toString()
+                    )
+                    userRequest!!.user = UserRegisterDataRequest()
 
-            if (userRequest != null) {
-                userRequest!!.personalID = personalIDImageUrl
-                userRequest!!.address = UserAddressDataRequest(
-                    countryCode = binding.registerDeliverLayout.layoutRegisterProfileCountryCode.EditTextFieldValidation.text.toString(),
-                    houseNumber = binding.registerDeliverLayout.layoutRegisterUserHouseNumber.EditTextFieldValidation.text.toString(),
-                    place = binding.registerDeliverLayout.layoutRegisterProfilePlace.EditTextFieldValidation.text.toString(),
-                    street = binding.registerDeliverLayout.layoutRegisterUserStreet.EditTextFieldValidation.text.toString(),
-                    zip = binding.registerDeliverLayout.layoutRegisterProfileZipCode.EditTextFieldValidation.text.toString()
-                )
-                userRequest!!.user = UserRegisterDataRequest()
-
-                viewModel.saveUser(userRequest!!)
+                    viewModel.saveUser(userRequest!!)
+                }
             }
         }
 //        (activity as KiimoMainNavigationActivity).viewModel.sav
@@ -107,6 +103,29 @@ class ChangeAccountTypeDialog : BaseKiimoDialog() {
         binding.registerDeliverLayout.activityRegisterProfileVerificationField.TextFieldPersonalID.setOnClickListener {
             MediaManager.showMediaOptionsDialog(requireActivity() as IMediaManagerImages)
         }
+
+        atachListenerToField(
+            binding.registerDeliverLayout.layoutRegisterUserStreet.EditTextFieldValidation,
+            ::validateStreet
+        )
+        atachListenerToField(
+            binding.registerDeliverLayout.layoutRegisterUserHouseNumber.EditTextFieldValidation,
+            ::validateHouseNumber
+        )
+        atachListenerToField(
+            binding.registerDeliverLayout.layoutRegisterProfileZipCode.EditTextFieldValidation,
+            ::validateZipCode
+        )
+        atachListenerToField(
+            binding.registerDeliverLayout.layoutRegisterProfilePlace.EditTextFieldValidation,
+            ::validatePlace
+        )
+
+        atachListenerToField(
+            binding.registerDeliverLayout.layoutRegisterProfileCountryCode.EditTextFieldValidation,
+            ::validateCountryCode
+        )
+
     }
 
     fun onNewBitmap(bitmap: Bitmap) {
@@ -119,6 +138,115 @@ class ChangeAccountTypeDialog : BaseKiimoDialog() {
 
     fun setServerImageUrl(imageUrl: String) {
         personalIDImageUrl = imageUrl
+        validateImageField()
     }
 
+    fun validations(): Boolean {
+
+        val validHouseNumber = validateHouseNumber()
+        val validZipCode = validateZipCode()
+        val validPlace = validatePlace()
+        val validCountryCode = validateCountryCode()
+        val validStreet = validateStreet()
+        val validPhoto = validateImageField()
+
+
+        val valid =
+            validZipCode && validPlace && validCountryCode && validHouseNumber && validStreet && validPhoto
+        return valid
+
+    }
+
+    private fun validateHouseNumber(): Boolean {
+        val valid =
+            binding.registerDeliverLayout.layoutRegisterUserHouseNumber.EditTextFieldValidation.textValue()
+                .isNotBlank()
+        displayValidationStatus(binding.registerDeliverLayout.layoutRegisterUserHouseNumber, valid)
+
+        return valid
+    }
+
+    private fun validateZipCode(): Boolean {
+        val valid =
+            binding.registerDeliverLayout.layoutRegisterProfileZipCode.EditTextFieldValidation.textValue()
+                .isNotBlank()
+        displayValidationStatus(binding.registerDeliverLayout.layoutRegisterProfileZipCode, valid)
+
+        return valid
+    }
+
+    private fun validateStreet(): Boolean {
+        val valid =
+            binding.registerDeliverLayout.layoutRegisterUserStreet.EditTextFieldValidation.textValue()
+                .isNotBlank()
+        displayValidationStatus(binding.registerDeliverLayout.layoutRegisterUserStreet, valid)
+
+        return valid
+    }
+
+    private fun validatePlace(): Boolean {
+        val valid =
+            binding.registerDeliverLayout.layoutRegisterProfilePlace.EditTextFieldValidation.textValue()
+                .isNotBlank()
+        displayValidationStatus(binding.registerDeliverLayout.layoutRegisterProfilePlace, valid)
+
+        return valid
+    }
+
+
+    private fun validateCountryCode(): Boolean {
+        val valid =
+            binding.registerDeliverLayout.layoutRegisterProfileCountryCode.EditTextFieldValidation.textValue()
+                .isNotBlank()
+        displayValidationStatus(
+            binding.registerDeliverLayout.layoutRegisterProfileCountryCode,
+            valid
+        )
+
+        return valid
+    }
+
+    private fun validateImageField(): Boolean {
+
+        val valid = personalIDImageUrl.isNotBlank()
+
+        displayValidationStatus(
+            binding.registerDeliverLayout.activityRegisterProfileVerificationField,
+            valid
+        )
+        return valid
+
+    }
+
+    protected fun displayValidationStatus(viewValidator: View, stateValid: Boolean) {
+        viewValidator.imageViewValidation.visibility =
+            if (!stateValid) View.VISIBLE else View.INVISIBLE
+        viewValidator.textViewFieldRequired.visibility =
+            if (!stateValid) View.VISIBLE else View.INVISIBLE
+    }
+
+    protected fun atachListenerToField(field: EditText, listener: () -> Boolean) {
+        field.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    listener()
+                }
+
+                override fun beforeTextChanged(
+                        s: CharSequence?, start: Int, count: Int, after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+            }
+        )
+    }
+
+}
+
+private fun EditText.textValue(): String {
+    return this.text.toString()
 }
