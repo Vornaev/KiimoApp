25fbaf79c9fb5fc2589c32eda278eb33e6af6cc1
viktor manev <vice.eu@gmail.com>
2020-06-28 19:22:10 +0200

    fix

--- app/src/main/java/org/kiimo/me/main/MainActivity.kt
+++ app/src/main/java/org/kiimo/me/main/MainActivity.kt
@@ -123,6 +123,8 @@ class MainActivity : KiimoMainNavigationActivity(),
             (messageReceiver),
             IntentFilter(AppConstants.FIREBASE_BROADCAST)
         )
+
+        showPriceBreakdownDialog(DeliveryPrice("0.0", "100", "807", 12.2, 30.1, 20, 12.2, 100))
     }
 
     override fun onStop() {
